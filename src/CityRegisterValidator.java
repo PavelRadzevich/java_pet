02b88158cfaf610af2ffceb1ff619cd8b25152f0
PavelRadzevich <pavel.radzevich@gmail.com>
2022-12-29 05:40:15 +0300

    Video 9. delete gitignor from .idea

--- src/CityRegisterValidator.java
+++ src/CityRegisterValidator.java
@@ -1,5 +1,6 @@
 public class CityRegisterValidator {
     String hostName;
+    String login;
 
     AnswerCityRegister checkCityRegister(StudentOrder so) {
         System.out.println("CityRegister is running" + hostName);
